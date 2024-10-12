a207124624eec698843bf04698f8a290bf83b95e
cuifm <cuifm@ramaxel.com>
2024-10-12 10:48:07 +0800

    1. support the double/triple/quadruple command with {'x', 'm', 'v' ...} 2. use uefi pciio protocol not cf8 for scan more device. 3. easy build bat.

--- PciUtilsPkg/config.h
+++ PciUtilsPkg/config.h
@@ -8,10 +8,11 @@
 
 #define PCI_IDS "pci.ids"
 #define PCI_OS_GNU
-#define PCILIB_VERSION "3.6.2"
+#define PCILIB_VERSION "3.13.0"
 #define PCI_PATH_IDS_DIR "."
 #define PCI_HAVE_STDINT_H
 #define PCI_HAVE_PM_INTEL_CONF
+#define PCI_HAVE_64BIT_ADDRESS
 
 //BUGBUG: A crude ugly smelly patch.
 #undef stderr
